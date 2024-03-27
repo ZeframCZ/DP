1bb96f2a82e41004e3f073a11b0656c805d370ee
JiCech <36083916+ZeframCZ@users.noreply.github.com>
2024-03-27 13:22:58 +0100

    Update sql_print.py

--- sql_print.py
+++ sql_print.py
@@ -1,11 +1,8 @@
-"""Prints SQL data"""
+"""Collects MQTT data into SQL"""
 import sqlite3
-from datetime import datetime
 
 con = sqlite3.connect("mqtt_data.db")
 cur = con.cursor()
-
-
 
 res = cur.execute("SELECT day, month, year, time, topic, content FROM messages")
 
