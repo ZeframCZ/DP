bc4619b21b1f4a5538327ff44da7f8af56fa71f0
JiCech <36083916+ZeframCZ@users.noreply.github.com>
2024-03-31 20:55:38 +0200

    Update main.ino

--- pico/main.ino
+++ pico/main.ino
@@ -20,14 +20,18 @@ AsyncMqttClient mqttClient = set_mqtt();
 
 void setup() {
   Serial.begin(9600);
-  status = connect_wifi("____", "____");
+  status = connect_wifi("Cechovi", "Cechovi01");
   connect_mqtt(&mqttClient);
 }
 
 
-char input[500];
-bool send_periodically = false;
+char input[500]={'\0'};
+char per_payload[500]={'\0'};
+char per_topic[500]={'\0'};
+bool per_send = false;
 int pulish_time = 1000;
+
+
 unsigned long the_time = 0;
 unsigned long cur_time;
 
@@ -53,16 +57,19 @@ void loop() {
     //Publish a single message
     if (strstr(input, "pub") != 0){
       blink(3, 100);
-      char arg_payload[500];
-      char arg_topic[500];
+      char arg_payload[500]={'\0'};
+      char arg_topic[500]={'\0'};
+      sprintf(arg_payload, "%s", "None");
+      sprintf(arg_topic, "%s", "None");
       find_str(input, 'm', arg_payload);
-      //find_str(input, 't', arg_topic);
+      find_str(input, 't', arg_topic);
+      Serial.println(arg_topic);
+      Serial.println(arg_payload);
 
-
-      if (strlen(arg_payload)!=0){
-        //mqttClient.publish(arg_topic, 1, false, arg_payload);
-        mqttClient.publish("rpi/pico/test", 1, false, arg_payload);
-        Serial.println(arg_payload);
+      mqttClient.publish(arg_topic, 1, false, arg_payload);
+      
+      for(int i = 0 ; i < 500 ; i++){
+        input[i] = '\0';
       }
     }
     
@@ -70,21 +77,26 @@ void loop() {
     //publish a message every x seconds
     if (strstr(input, "per") != 0){
       blink(2, 500);
-      int arg = find_int(input, 't');
+      int arg = find_int(input, 's');
+      sprintf(per_payload, "%s", "None");
+      sprintf(per_topic, "%s", "None");
+      find_str(input, 'm', per_payload);
+      find_str(input, 't', per_topic);
+
       if (arg!=0){
         pulish_time = arg*1000;
       }
       the_time = cur_time+pulish_time;
-      send_periodically = !send_periodically;
+      per_send = !per_send;
     }
-
+    //connect to mqtt
     if (strstr(input, "con") != 0){
       connect_mqtt(&mqttClient);
     }
     //DEBUG
     if (strstr(input, "deb") != 0){
       blink(5, 50);
-      Serial.println("---DEBUG---");
+      Serial.println("\n---DEBUG---");
       Serial.println("the_time");
       Serial.println(the_time);
       Serial.println("cur_time");
@@ -92,21 +104,30 @@ void loop() {
       Serial.println("pulish_time");
       Serial.println(pulish_time);
       Serial.println("send_periodically");
-      Serial.println(send_periodically);
+      Serial.println(per_send);
       Serial.println("status");
       Serial.println(status);
-      Serial.println("---DEBUG---");
+      Serial.println("---DEBUG---\n");
+    }
+    //HELP
+    if (strstr(input, "help") != 0){
+      blink(2, 50);
+      Serial.println("\n---HELP---");
+      Serial.println("(Publish a single message)       pub -t topic -m payload");
+      Serial.println("(Publish a message periodically) per -s time(s) -m payload -t topic");
+      Serial.println("                                 help");
+      Serial.println("(Debug)                          deb");
+      Serial.println("(connect to mqtt broker)         con");
+      Serial.println("---HELP---\n");
     }
     //clear input
     for(int i = 0 ; i < 500 ; i++){
       input[i] = '\0';
     }
   }
-  //Serial.println(input, DEC);
-
   
   //Send periodic message
-  if (cur_time >= the_time && send_periodically == true){
+  if (cur_time >= the_time && per_send == true){
     if (status == 3){
       
       char pt[11];
@@ -114,14 +135,9 @@ void loop() {
         sprintf(pt, "%d", pulish_time);
         Serial.println(pt);
       }
-      //wchar_t
-      char message[500] = {'\0'};
-      jointnstr(message, "This message is sent ", 500);
-      jointnstr(message, pt, 500);
-      jointnstr(message, "ms apart.", 500);
-      mqttClient.publish("test/picoW", 1, false, message);
+      mqttClient.publish(per_topic, 1, false, per_payload);
     }else{
-      status = connect_wifi("____", "____");
+      status = connect_wifi("Cechovi", "Cechovi01");
       connect_mqtt(&mqttClient);
     }
     the_time = cur_time+pulish_time;
